0344c17681355e00073c65b774608fa6d950d17f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-22 16:09:07 -0400

    feat: implement loadEnvironmentFiles function for loading environment variables

---
+++ src/util/load_environment.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import { expand } from 'dotenv-expand';
+import { existsSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+import type { EnvironmentOptions } from '@src/type/environment_options.js';
+
+const loadEnvironmentFiles = (options?: EnvironmentOptions) => {
+  const currentState = process.env.NODE_ENV ?? 'development';
+  const environmentFilePath = resolve(process.cwd(), options?.path ?? '', options?.fileName ?? `.env.${currentState}`);
+
+  if (existsSync(environmentFilePath)) {
+    const config = dotenv.config({ path: environmentFilePath, quiet: true });
+    expand(config);
+    return { path: environmentFilePath, state: currentState };
+  }
+
+  throw new Error(`File not found: ${environmentFilePath}`);
+};
+
+export { loadEnvironmentFiles };
